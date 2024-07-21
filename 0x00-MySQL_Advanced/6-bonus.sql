fc86fa2dcd80111f63a55ab8c681bb3b72e93c42
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-21 23:29:44 +0100

    Create answer file

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,8 @@
+-- An SQL script that creates a stored procedure
+-- that adds a new correction for a student
+-- This procedure is taking in 3 inputs:
+-- 'user_id': a users.id value
+--      (you can assume user_id is linked to an existing users)
+-- 'project_name': a new or already exists projects - if no
+--      projects.name found in the table, you should create it
+-- 'score': the score value for the correction
