04c69d1088272d726609225abd81d959c48adf6e
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-24 15:49:17 +0100

    Update documentation

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -14,5 +14,8 @@ import pymongo
 
 
 def insert_school(mongo_collection, **kwargs):
-    """inserts a new document in a collection"""
+    """
+    Inserts a new document in a collection
+    using variable arguments in Python
+    """
     return mongo_collection.insert(kwargs)
