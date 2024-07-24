fc7ecc51a83ef268c746630d04e1c9aa50c22fcc
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-24 15:49:18 +0100

    A Python script that returns the list of schools having a specific topic

---
+++ 0x01-NoSQL/11-schools_by_topics.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+A Python function that returns the list of school
+having a specific topic:
+
+Prototype: def schools_by_topic(mongo_collection, topic):
+mongo_collection will be the pymongo collection object
+topic (string) will be topic searched
+"""
+
+
+# Import statements
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns the list of school having a specific topic
+    """
+    return mongo_collection.find({"topics": topic})
