fe4832785b8d3be4f5f2f2e0034c8631ccb8ca8f
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-29 23:25:26 +0100

    Update code with code to automatically parameterize Cache.get

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,7 +9,7 @@ and as a cache also
 # Import statements
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -32,3 +32,30 @@ class Cache:
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+        """
+        Method that takes a key argument
+        and returns the data
+        """
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, data: bytes) -> str:
+        """
+        automatically parametrizes Cache.get
+        with the correct
+        conversion function
+        """
+        value = self._redis.get(data)
+        return value.decode('utf-8')
+
+    def get_int(self, data: bytes) -> int:
+        """
+        automatically parametrizes Cache.get
+        with the correct conversion function
+        """
+        value = self._redis.get(data)
+        return int(value)
