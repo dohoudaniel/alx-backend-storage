f9b9266abde89b5ee934dc9eb7f99b462f93a830
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-30 23:54:59 +0100

    Update file from intrsnet

--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -2,15 +2,29 @@
 """
 Main file
 """
-import redis
-
+# Code for task 0 and 1
+# import redis
+#
+# Cache = __import__('exercise').Cache
+#
+# cache = Cache()
+#
+# data = b"hello"
+# key = cache.store(data)
+# print(key)
+#
+# local_redis = redis.Redis()
+# print(local_redis.get(key))
+#
+#
+# Code for task 2
 Cache = __import__('exercise').Cache
 
 cache = Cache()
 
-data = b"hello"
-key = cache.store(data)
-print(key)
+cache.store(b"first")
+print(cache.get(cache.store.__qualname__))
 
-local_redis = redis.Redis()
-print(local_redis.get(key))
+cache.store(b"second")
+cache.store(b"third")
+print(cache.get(cache.store.__qualname__))
