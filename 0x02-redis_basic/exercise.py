fb5462452d83ab53a909da02f72f3f02b45fb45f
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-31 07:43:35 +0100

    A wrapper function that stores history

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -30,6 +30,25 @@ def count_calls(method: Callable) -> Callable:
 
     return wrapper
 
+def call_history(method: Callable) -> Callable:
+    """
+    Store history of inputs and
+    outputs for a particular function
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        A wrapper for the decorated
+        function call_history
+        """
+        input = str(args) # Normalizing
+        self._redis.rpush(method.__qualname__ + ":inputs", input)
+        output = str(method(self, *args, **kwargs))
+        self._redis.rpush(method.__qualname__ + ":outputs", output)
+        return output
+
+    return wrapper
+
 
 class Cache:
     """
@@ -45,6 +64,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Method that takes a data argument
